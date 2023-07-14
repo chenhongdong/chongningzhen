bf657d8f9eab7470019be358e743820e6a3c38c1
chenhongdong <513948670@qq.com>
2023-07-14 18:26:09 +0800

    '我的页开发'

--- src/api/config.js
+++ src/api/config.js
@@ -8,10 +8,16 @@ export const GET_SCENIC_SPOT_DETAIL = BASE_URL + '/api/posts/home/scenic_spot/fi
 export const GET_AD_BANNER = BASE_URL + '/api/meta/home/banner/advertising/findByAliasLoadSubList'
 // 通过最小时间获取预约课程
 export const GET_COURSE = BASE_URL + '/api/posts/home/booking_plan/findPagingByMinDate'
+// 添加预约
+export const ADD_STUDY_USER = BASE_URL + '/api/posts/home/user_booking/addByCurrentUser'
+// 微信OpenId
+export const OPENID_VERIFY = BASE_URL + '/api/user/home/wxAuth/loginByWxOpenId'
 
 export default {
     GET_SCENIC_SPOT_FIND,
     GET_SCENIC_SPOT_DETAIL,
     GET_AD_BANNER,
-    GET_COURSE
+    GET_COURSE,
+    ADD_STUDY_USER,
+    OPENID_VERIFY
 }
