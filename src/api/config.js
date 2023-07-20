8ab8ef26aa985a28067045ec88cc67ab55b644e1
chenhongdong <513948670@qq.com>
2023-07-20 22:50:15 +0800

    '整体调整'

--- src/api/config.js
+++ src/api/config.js
@@ -2,8 +2,12 @@
 export const BASE_URL = 'https://hongselvyou.lndx-study.beihero.com'
 // 景点查询分页
 export const GET_SCENIC_SPOT_FIND = BASE_URL + '/api/posts/home/scenic_spot/findPaging'
+// 景点查询分页加载按钮
+export const GET_SCENIC_SPOT_FIND_LOAD = BASE_URL + '/api/posts/home/scenic_spot/findPagingLoadBtn'
 // 景点查询详情页传入对应id
 export const GET_SCENIC_SPOT_DETAIL = BASE_URL + '/api/posts/home/scenic_spot/find'
+// 查询通过id加载按钮     
+export const GET_SCENIC_SPOT_DETAIL_LOAD = BASE_URL + '/api/posts/home/scenic_spot/findLoadBtn'
 // 通过别名获取banner
 export const GET_AD_BANNER = BASE_URL + '/api/meta/home/banner/advertising/findByAliasLoadSubList'
 // 通过最小时间获取预约课程
@@ -23,7 +27,9 @@ export const REFRESH_TOKEN = BASE_URL + '/api/user/home/wxAuth/refreshToken'
 
 export default {
     GET_SCENIC_SPOT_FIND,
+    GET_SCENIC_SPOT_FIND_LOAD,
     GET_SCENIC_SPOT_DETAIL,
+    GET_SCENIC_SPOT_DETAIL_LOAD,
     GET_AD_BANNER,
     GET_COURSE,
     OPENID_VERIFY,
